11aa810959f53ee3b0b400cf03988eabb036190d
AbhinnVerma <63635091+AbhinnVerma@users.noreply.github.com>
2020-07-11 19:15:31 +0530

    Add files via upload
    Notes app v-1.0

---
+++ Notes app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+notes = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        note = request.form.get('note')
+        notes.append(note)
+
+    return render_template('index.html', notes=notes)
+
+
+
+#Check CS50 Flask for session module
